aa8db5705182f832e4878c9a621ebd92612be365
Suraj S G Dhanva <surajsgd23@gmail.com>
2026-01-08 11:51:42 +0530

    Add all available branches

---
+++ Client/src/data/allBranches.ts
@@ -0,0 +1,22 @@
+const Branches = [
+  "Biotechnology",
+  "Civil Engineering",
+  "Construction Technology and Management",
+  "Environmental Engineering",
+  "Industrial and Production Engineering",
+  "Mechanical Engineering",
+  "Polymer Science and Technology",
+  "Computer Science and Engineering",
+  "Electrical and Electronics Engineering",
+  "Electronics and Communication Engineering",
+  "Electronics and Instrumentation Engineering",
+  "Information Science and Engineering",
+  "Computer Science and Business Systems",
+  "MBA",
+  "Computer Applications",
+  "Chemistry",
+  "Mathematics",
+  "Physics"
+];
+
+export default Branches;
